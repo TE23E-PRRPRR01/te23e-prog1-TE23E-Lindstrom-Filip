29f576b243545b56b247d396fe0fda42a6c0e0ae
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2024-08-26 14:31:51 +0200

    tränat på cw, cr och svenska tecken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Hej, världen!");
+Console.WriteLine("Jag vill hem");
+Console.WriteLine("Vad heter du?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej" + namn);
+
+
+
+Console.WriteLine("Vad gör du på fritiden?");
+string L = Console.ReadLine();
+Console.WriteLine("vad roligt, Jag med!");
+
+
+Console.BackgroundColor= ConsoleColor.DarkCyan;
+Console.ForegroundColor = ConsoleColor.DarkBlue;
