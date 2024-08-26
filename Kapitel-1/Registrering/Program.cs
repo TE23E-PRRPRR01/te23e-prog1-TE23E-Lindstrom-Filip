ef1923026e310cebd506af0fafc0b4028577bbec
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2024-08-26 14:33:18 +0200

    träant cw, cr och svenska tecken

---
+++ Kapitel-1/Registrering/Program.cs
@@ -0,0 +1,13 @@
+﻿// Program för att registrera förnamn och efternamn
+using System.ComponentModel;
+
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding =System.Text.Encoding.Unicode;
+Console.WriteLine("Program för att registrera förnamn och efternamn");
+Console.Write("Ange förnamn: ");
+string förnamn = Console.ReadLine();
+Console.Write("ange efternamn: ");
+string efternamn = Console.ReadLine();
+Console.WriteLine("Hej! " + förnamn + " " + efternamn);
+Console.WriteLine(%"hej! {förnamn} {efternamn}");
