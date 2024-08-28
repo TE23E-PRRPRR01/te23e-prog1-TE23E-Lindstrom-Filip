5a30d71db47024888aa76d5cfa262bcdef81e8be
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2024-08-28 14:42:06 +0200

    Banger Fotbolls Lucktext

--- Kapitel-1/Registrering/Program.cs
+++ Kapitel-1/Registrering/Program.cs
@@ -10,4 +10,15 @@ string förnamn = Console.ReadLine();
 Console.Write("ange efternamn: ");
 string efternamn = Console.ReadLine();
 Console.WriteLine("Hej! " + förnamn + " " + efternamn);
-Console.WriteLine(%"hej! {förnamn} {efternamn}");
+Console.WriteLine($"hej! {förnamn} {efternamn}");
+
+// är det filip som loggar in?
+if (förnamn == "filip")
+{
+    Console.WriteLine("vad trevligt att du är tillbaka");
+    
+}
+if (efternamn == "lindström")
+{
+    Console.WriteLine("vackert efternamn!");
+}
