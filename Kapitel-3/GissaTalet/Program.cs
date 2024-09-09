31409fae9d95a62efaeb43ca25f843bbcc3ef4bc
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2024-09-09 14:50:12 +0200

    Tränat på heltal och konvertering med inte parse och slumptal

---
+++ Kapitel-3/GissaTalet/Program.cs
@@ -0,0 +1,34 @@
+﻿// Ett litet spel - gissa ett hemligt heltal
+using System.ComponentModel.Design;
+
+Console.Clear();
+Console.WriteLine("Ett litet spel - gissa ett hemligt heltal");
+
+// slumpa ett tal
+int slumptalet = Random.Shared.Next (1, 101); // 1-100 man har med minsta värde men inte största
+
+Console.WriteLine($"Slumptalet är {slumptalet}");
+//Console.WriteLine("Slumptalet är" + "slumptalet");
+
+Console.Write("Gissa ett tal 1-100: ");
+// läs in texten, t ex "100"
+string gissningText = Console.ReadLine();
+
+// kovertera text till heltal
+int gissning = int.Parse(gissningText);
+
+if (gissning >= 70)
+{
+    Console.WriteLine("Fel, talet är lägre");
+}
+
+else if (gissning > slumptalet)
+{
+    Console.WriteLine("för högt");
+}
+else
+{
+    Console.WriteLine("för lågt!");
+}
+
+
---
+++ Kapitel-3/Åldergränser/Program.cs
@@ -0,0 +1,32 @@
+﻿// 
+Console.Clear();
+Console.WriteLine("Ålder vid första mord");
+
+
+Console.Write("Ange Ålder");
+
+string ÅlderText = Console.ReadLine();
+
+int Ålder = int.Parse(ÅlderText);
+
+
+if (Ålder <= 5)
+{
+    Console.WriteLine("Imponerande, Hur fan lyckas man?");
+}
+
+else if (Ålder <= 9)
+   {
+     Console.WriteLine("Bra jobbat, stabil ålder");
+   }
+
+else if(Ålder <= 14)
+   {
+    Console.WriteLine("Lite sent men det funkar");
+   }
+else if (Ålder >= 18)
+{
+    Console.WriteLine("Varför gjorde du det då är för HELVETE straffmyndig");
+}
+
+
