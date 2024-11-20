9d182f08bcf29b9fd44d26ea350f4e90b890bf72
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2024-11-20 15:00:42 +0100

    Trämnat for loop till bakgrundmusik

---
+++ Kapitel-4/ForBonanza/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+Console.Clear();
+
+
+Console.Write("ange startvärde:");
+int startvärde = int.Parse(Console.ReadLine());
+
+Console.Write("ange slutvärde:");
+int slutvärde = int.Parse(Console.ReadLine());
+
+int sum1 = 0;
+int sum2 = 0;
+
+Console.Write($"jämna siffror:");
+for (int i = startvärde; i < slutvärde +1; i++)
+{
+   if (i % 2 == 0)
+   {
+        Console.WriteLine($" {i}");
+   }
+}
+
+Console.Write($"Udda siffror: ");
+for (int i = startvärde; i < slutvärde +1; i++)
+{
+   if (i % 2 != 0)
+   {
+        Console.WriteLine($" {i}");
+   }
+}
