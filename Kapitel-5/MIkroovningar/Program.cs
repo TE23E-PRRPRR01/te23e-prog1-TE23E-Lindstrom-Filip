6b5546a52d3077083eecedd8c61a7c54eac5ad63
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2024-11-25 14:50:14 +0100

    övat på övning

---
+++ Kapitel-5/MIkroovningar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices.Marshalling;
+
+Console.Clear();
+
+List<string> namn = ["Anna", "Björn", "Cecilia"];
+
+Console.WriteLine($"Nuvarande lista: {string.Join(", ", namn)}");
+
+Console.WriteLine($"Ange namn att lägga till:");
+string adnamn = Console.ReadLine();
+namn.Add(adnamn);
+
+Console.WriteLine($"Listan efter tillagtnamn: {string.Join(", ", namn)}");
+
+
+while (true)
+{
+    Console.WriteLine($"Ange namn att ta bort till:");
+    string removenamn = Console.ReadLine();
+
+    if (namn.Contains("removenamn"))
+    {
+        namn.Remove(removenamn);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Namnet fanns inte i listan");
+    }
+    
+    Console.WriteLine($"Listan efter bortaget namn: {string.Join(", ", namn)}");
+
+}
