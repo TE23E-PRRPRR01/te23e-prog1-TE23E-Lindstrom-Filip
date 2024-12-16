80ceaebd0c40438e393382770cb5c30be0e4221f
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2024-12-16 14:47:59 +0100

    gjort övning

---
+++ Kapitel-5/Listor 2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+Console.WriteLine("Allt om listor");
+
+
+//Skapa en tom lista
+List<string> listanamn = [];
+
+//Infoga i listan
+listanamn.Add("Tyr");
+listanamn.Add("hej");
+Console.WriteLine(listanamn.Count);
+
+//skriv ut listan (går inte)
+Console.WriteLine(listanamn);
+
+//skriv ut lista med loop
+foreach (var namn in listanamn)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+//Skriv ut första elementet i listan (index = 0)
+Console.WriteLine($"första elementet: {listanamn[0]}");
+
+//Skriv ut två namn ur listan 
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"- {listanamn[i]}");
+}
+
+//skriv ut alla namn i listan med for-loop
+for (int i = 0; i < listanamn.Count; i++)
+{
+    Console.WriteLine($"{i}: {listanamn[i]}");
+}
+
+
---
+++ Kapitel-5/mikroovningar 2/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+
+List<string> listaActionSpel = [];
+
+listaActionSpel.Add("avengers");
+listaActionSpel.Add("starwars");
+
+foreach (var namn in listaActionSpel)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+List<string> listaäventyrsSpel = [];
+
+listaäventyrsSpel.Add("COD");
+listaäventyrsSpel.Add("CS");
+
+foreach (var namn in listaäventyrsSpel)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+
+Console.WriteLine("Nu ska du fylla på listan med två äventyrs spel");
+
+for (int i = 0; i < 2;)
+{
+    i++;
+    Console.Write($"Spel {i} :");
+    listaäventyrsSpel.Add(Console.ReadLine());
+}
+
+Console.WriteLine("Nu ska du fylla på listan med två action spel");
+
+for (int i = 0; i < 2;)
+{
+    i++;
+    Console.Write($"Spel {i} :");
+    listaActionSpel.Add(Console.ReadLine());
+}
+
+Console.WriteLine("Nus er listan ut såhär");
+
+foreach (var namn in listaActionSpel)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+
+Console.WriteLine("Nu ser listan ut såhär");
+
+foreach (var namn in listaäventyrsSpel)
+{
+    Console.WriteLine($"- {namn}");
+}
