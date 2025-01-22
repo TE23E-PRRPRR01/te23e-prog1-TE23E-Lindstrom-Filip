dc467a72400e15c11e41dd90d18a17e73e57cd8a
Filip Lindström <Filip.lindstrom@elev.ntig.ga.se>
2025-01-22 15:05:47 +0100

    tränat på metoder och top down strategin

---
+++ kapitel 6/Kalkylator/Program.cs
@@ -0,0 +1,66 @@
+﻿
+/******************************
+**********M A I N *************
+******************************/
+while (true)
+{
+
+    VisaMeny();
+
+    string val = Console.ReadLine();
+
+
+    if (val == "1")
+    {
+        Adition();
+    }
+
+    else if (val == "2")
+    {
+
+    }
+
+    else if (val == "3")
+    {
+
+    }
+   
+    else if (val == "4")
+    {
+
+    }
+
+     else if (val == "5")
+    {
+
+    }
+    
+    else
+    {
+        Console.WriteLine("""
+        Du angav felaktigt värde
+        Försök igen!
+        """);
+    }
+}
+
+
+
+
+/******************************
+**********M A I N *************
+******************************/
+
+
+
+static void VisaMeny()
+{
+    Console.WriteLine("""
+Vad vill du göra
+1.Addition
+2.subraktion
+3.multiplikation
+4.Division
+5.Avsluta programmet
+""");
+}
---
+++ kapitel 6/MedelandeApp/Program.cs
@@ -0,0 +1,71 @@
+﻿/* *************************
+*********M A I N ***********
+***************************/
+
+VisaPresentation();
+
+while (true)
+{
+    VisaMeny();
+    String val = Console.ReadLine();
+
+    if (val == "1")
+    {
+        SparaMedelande();
+    }
+    else if (val == "2")
+    {
+        LäsaMeddelande();
+    }
+    else if (val == "3")
+    {
+        Console.WriteLine("Tack för idag!");
+        break;
+    }
+    else
+    {
+
+    }
+
+}
+
+
+/* *************************
+*********M A I N ***********
+***************************/
+static void VisaPresentation()
+{
+    Console.Clear();
+    Console.WriteLine("""
+    * Program för att spara meddelande på disken *
+    """);
+}
+
+static void VisaMeny()
+{
+    Console.WriteLine("Visa meny");
+
+    Console.WriteLine("""
+    1.Spara meddelande
+    2.läsa meddelande
+    3.avsluta programmet
+    """);
+}
+
+static void SparaMedelande()
+{
+    
+    Console.Write("Ange ett meddelande:   ");
+    string meddelande = Console.ReadLine();
+    File.WriteAllText("meddelande.txt", meddelande);
+    Console.WriteLine("ditt meddelande har sparats!");
+}
+
+static void LäsaMeddelande(string filen)
+{
+    string meddelande = File.ReadAllText (filen);
+    Console.WriteLine("Ditt senaste ");
+    Console.WriteLine("____________________");
+    Console.WriteLine(meddelande);
+    Console.WriteLine("_______________");
+}
